3ec9c1b353692ae11c005695907f6849df405fc6
Clinton Jenkins <jenkins.333@gmail.com>
2017-08-28 21:27:02 -0600

    Files created doing exercises in Chapter 2.

---
+++ main.hs
@@ -0,0 +1,12 @@
+fn1       = x * 3 + y
+  where x = 3
+        y = 1000
+
+fn2 = x * 5
+  where x = 10 * 5 + y
+        y = 10
+
+fn3 = z / x + y
+  where x = 7
+        y = negate x
+        z = y * 10
